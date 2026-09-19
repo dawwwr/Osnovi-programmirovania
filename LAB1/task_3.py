6008ef4e4ba8cd7494da5ef820c0d05ec64581b8
dawwwr <znbzhjybdm@privaterelay.appleid.com>
2026-09-20 00:53:58 +0300

    Update task_3.py

--- LAB1/task_3.py
+++ LAB1/task_3.py
@@ -9,3 +9,10 @@ print(x % y, type(x % y))
 print(x ** y, type(x ** y))
 
 print(2 ** 1000)
+
+import math
+result = 0.1 + 0.2
+print(result)
+print(result == 0.3)
+print(result - 0.3)
+print(math.isclose(result, 0.3))
