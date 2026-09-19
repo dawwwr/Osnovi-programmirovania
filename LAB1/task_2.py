1f92ac72f3a757d10f260a855e2b4905decac661
dawwwr <znbzhjybdm@privaterelay.appleid.com>
2026-09-19 14:35:57 +0300

    Update task_2.py

--- LAB1/task_2.py
+++ LAB1/task_2.py
@@ -1,1 +1,16 @@
+#Прогноз:
+#a == b True
+#a is b True
+#a == c True
+#a is c False
 
+a = 1000
+b = a
+c = int("1000")
+
+print("Типы:", type(a), type(b), type(c))
+print("Идентификаторы:", id(a), id(b), id(c))
+print("a == b:", a == b)
+print("a is b:", a is b)
+print("a == c:", a == c)
+print("a is c:", a is c)
